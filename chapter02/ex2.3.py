c9cc591e4cfa6f94e6baab01e60a4fcb24e37df4
DaBigJoe <joseph.early@btinternet.com>
2018-06-16 12:22:47 +0000

    Fixed rendering for ex2.3 and added .p files to .gitignore.

--- chapter02/ex2.3.py
+++ chapter02/ex2.3.py
@@ -1,17 +1,22 @@
 from chapter02.BanditSolutions import runEGreedy, runAndPlot
+from matplotlib.pyplot import rc
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
 _n = 10
-_plays = 20000
+_plays = 30000
 _repeats = 2000
+
+rc('text', usetex=True)
+plt.rc('font', family='serif')
+mpl.rcParams.update({'font.size': 14})
 
 figure, axes = plt.subplots(figsize=(8, 8), nrows=3, ncols=1)
 
 print('Results')
-e01avg, e01rwd = runAndPlot(axes, _repeats, _plays, _n, runEGreedy, [0.1, "sample_average", 0], {}, 'e=0.1', useSave=True)
-e001avg, e001rwd = runAndPlot(axes, _repeats, _plays, _n, runEGreedy, [0.01, "sample_average", 0], {}, 'e=0.01', useSave=True)
-#runAndPlot(axes, _repeats, _plays, _n, runEGreedy, [0, "sample_average", 0], {}, 'e=0', useSave=True)
+e01avg_rwds, e01avg_optimals = runAndPlot(axes, _repeats, _plays, _n, runEGreedy, [0.1, "sample_average", 0], {}, 'e=0.1', useSave=True)
+e001avg_rwds, e001avg_optimals = runAndPlot(axes, _repeats, _plays, _n, runEGreedy, [0.01, "sample_average", 0], {}, 'e=0.01', useSave=True)
 
 axes[0].set_xlabel('Plays')
 axes[0].set_ylabel('Average reward')
@@ -19,7 +24,7 @@ axes[0].set_xlim(0, _plays)
 axes[0].legend(loc='lower right')
 
 axes[1].set_xlabel('Plays')
-axes[1].set_ylabel('% Optimal action')
+axes[1].set_ylabel('\% Optimal action')
 axes[1].set_xlim(0, _plays)
 axes[1].set_ylim(0, 100)
 axes[1].legend(loc='lower right')
@@ -30,12 +35,13 @@ axes[2].set_xlim(0, _plays)
 axes[2].legend(loc='lower right')
 
 figure, axis = plt.subplots(figsize=(8, 3), nrows=1, ncols=1)
-axis.plot(np.arange(0, _plays, 1), np.cumsum(e001avg)/np.cumsum(e01avg), color='k')
+axis.plot(np.arange(0, _plays, 1), e001avg_optimals/e01avg_optimals, color='k')
 axis.plot([0, _plays], [1.088, 1.088], linestyle='--', color='k')
+axis.plot([0, _plays], [1.0, 1.0], linestyle=':', color=[0.5, 0.5, 0.5])
 axis.set_xlabel('Plays')
-axis.set_ylabel('Method comparison')
+axis.set_ylabel('Optimality comparison')
 axis.set_xlim(0, _plays)
-axis.set_ylim(0.9, 1.1)
+axis.set_ylim(0.6, 1.2)
 
 plt.tight_layout()
 plt.show()
